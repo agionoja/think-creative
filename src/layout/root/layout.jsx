4b90038c112fa651e93d94d7c7e843d2cb4273e2
ahneyle007 <aneledarlington007@gmail.com>
2024-05-17 14:28:12 +0100

    servicesPage updated

--- src/layout/root/layout.jsx
+++ src/layout/root/layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const RootLayout = () => {
   return (
     <div>
-      <div className="text-x5l uppercase">Route is set up!</div>
+      {/* <div className="text-x5l uppercase">Route is set up!</div> */}
       <Outlet />
     </div>
   );
