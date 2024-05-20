c3f2c6e55ee8cd903e7b982a5ef6e215ea11c0fa
ahneyle007 <aneledarlington007@gmail.com>
2024-05-20 20:16:27 +0100

    desktop view completed

--- src/pages/services/servicesPage.jsx
+++ src/pages/services/servicesPage.jsx
@@ -1,5 +1,9 @@
-import campaignadd from "/src/assets/campaignadd.png"
 
+import campaignadd from "/src/assets/campaignadd.png"
+import homemeasure from "/src/assets/homemeasure.png"
+import repurpose from   "/src/assets/repurpose.png"
+import conor from "/src/assets/conor.png"
+import dunn from "/src/assets/dunn.png"
 
 const ServicesPage = () => {
   return( <div className="w-screen">
@@ -48,12 +52,91 @@ const ServicesPage = () => {
             </div>
             {/*End of Campaign layer */}
             {/*Measure layer */}
-            <div>
-              
-            </div>
+            <div className="h-[100vh] content-center items-center bg-black rounded-2xl">
+                <div className="flex items-center space-x-20 py-19 px-10 mx-10">
+                  <div className=" px-12">
+                    <div className="">
+                      <h5 className=""><button className="text-[10px] font-bold rounded-3xl px-3 py-2 m-1 mr-2 bg-[#c3c12f]">Measure</button></h5>
+                      <h1 className="text-4xl text-white font-bold py-3 pr-8">Understand what's working-and why</h1>
+                      <h3 className="text-[14px] text-white pr-8">No more guessing. Learn what content and creative elements resonate best with your audience and why. 
+                          With live reporting and post-campaign insights, you have the tools to make every campaign better.
+                      </h3>
+                    </div>
+                  </div>
+                  <img src={homemeasure} alt="XX" className="w-[27rem]"/>
+                </div>
+            </div>  
             {/*End of Measure layer */}
+            {/* Repurpose Layer */}
+            <div className="h-[100vh] content-center items-center rounded-t-2xl">
+                <div className="flex items-center space-x-20 py-19 px-10 mx-10">
+                  
+                  <img src={repurpose} alt="XX" className="w-[24rem]"/>
+                  
+                  <div className=" px-12">
+                    <div className="">
+                      <h5 className=""><button className="text-[10px] font-bold rounded-3xl px-3 py-2 m-1 mr-2 bg-[#c3c12f]">Repurpose</button></h5>
+                      <h1 className="text-4xl font-bold py-3 pr-8">Use your campaign content everywhere</h1>
+                      <h3 className="text-[14px] pr-8">Take the best-performing creator content and use it across different marketing channels, like
+                        out-of-home, print, and in-flight entertainment.  
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            {/* End of  Repurpose Layer */}
+            {/* Testimonial layer */}
+            <div className="h-[90vh] content-center justify-center items-center rounded-t-2xl bg-[#ebe4ef]">
+                <div className=" font-black float-end flex ">
+                  <button className=" mt-[28em] mr-[7rem] border-2 rotate-90 scale-y-[-1] scale-x-[-1]">
+                     <p className=" leading-tight inset-y-5 text-7xl text-gray-400 fixed">Testimonial</p>
+                  </button>
+                </div>   
+                <div className=" flex items-center justify-center space-x-1 py-5 px-20 mx-20">
+                  <div className="h-[70vh] mx-4 px-9 bg-white rounded-2xl content-center">
+                    <p className="font-sans font-bold text-[11px] w-[23em]"> 
+                      "On Think Creative Hub i'm able to partner with incredible brands
+                      allow me to be creative and use my voice and style when it comes to 
+                      creating content that I know will resonate with my audience and 
+                      make an impact."
+                    </p>
+                    <img alt="Llana Img" src={dunn} className="w-[15em] mb-9"/>
+                    <p className="text-[10px]">Llana Dunn - @seeingotherpeople</p>  
+                  </div>
+                  <div className="h-[70vh] mx-4 px-9 bg-white rounded-2xl content-center">
+                    <p className="font-sans font-bold text-xs w-[21em]">
+                      "They advocate for their creators on their platform from making sure 
+                       that we get paid fairly to hosting events that bring brands and creators
+                       together"
+                    </p>
+                    <img alt="XX" src={conor} className="w-[15em] pt-3 mb-9"/>
+                    <p className="text-[10px] mt-2">Conor Mckenzie</p>
+                  </div>
+                </div>
+             {/*<div className="rotate-90">
+                  <p className=" ">Testimonials</p>
+                </div> */ }
+                <div className="flex justify-center items-center pt-3 pl-7 mx-20">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </div>
+                    <div>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                      </svg>
 
+                    </div>
+                </div>
+                
+            </div>
+
+            {/* End of Testimonial layer */}
           </div>);
 };
 
 export default ServicesPage;
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
