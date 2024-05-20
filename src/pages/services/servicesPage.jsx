41c13364556ce100cbb6bf298f8a70e2f684046f
ahneyle007 <aneledarlington007@gmail.com>
2024-05-20 21:05:46 +0100

    deleted a comment

--- src/pages/services/servicesPage.jsx
+++ src/pages/services/servicesPage.jsx
@@ -113,9 +113,6 @@ const ServicesPage = () => {
                     <p className="text-[10px] mt-2">Conor Mckenzie</p>
                   </div>
                 </div>
-             {/*<div className="rotate-90">
-                  <p className=" ">Testimonials</p>
-                </div> */ }
                 <div className="flex justify-center items-center pt-3 pl-7 mx-20">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -137,6 +134,3 @@ const ServicesPage = () => {
 };
 
 export default ServicesPage;
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-</svg>
